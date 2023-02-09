954a9f94ec088ecad03d70f8237ddba0e9beb3d6
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-02-09 16:34:25 +0530

    addTodo Component completed

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,16 +1,40 @@
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
+import { GET_TODOS } from "./Todos";
 
 const ADD_TODO = gql`
   mutation AddTodo($type: String!) {
     addTodo(type: $type) {
-      idg
+      id
       type
     }
   }
 `;
 
 const AddTodo = () => {
-  return <div>AddTodo</div>;
+  let input;
+  const [addTodo, { error }] = useMutation(ADD_TODO, {});
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo({
+            variables: { type: input.value },
+          });
+          input.value = "";
+        }}
+      >
+        <input
+          ref={(node) => {
+            input = node;
+          }}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {error && <h2>error</h2>}
+    </div>
+  );
 };
 
 export default AddTodo;
